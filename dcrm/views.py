58660cfd6d63b1c1c55cdbba368cc867ce46e64a
Harold Gele <haroldgele03@gmail.com>
2024-05-03 19:50:17 +0800

    Working Login and Logout with Display Data

--- dcrm/views.py
+++ dcrm/views.py
@@ -7,29 +7,9 @@ from .models import Record
 def home(request):
     #DO SOMETHING
 
-    records = Record.objects.all()
-    if request.method == 'POST':
-        username = request.POST ['username']
-        password = request.POST ['password']
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "You Have Been Logged In!")
-            return redirect('home')
-        else:
-             messages.success(request, "There was an Error Logging In, Please Try Again")
-             return redirect('home')
-    else:
-
-        return render(request, 'home.html', {'records':records})
+    return render(request, 'home.html')
 
 
 def register(request):
 
     return render(request, 'register.html')
-
-def logout_user(request):
-    logout(request)
-    messages.success(request, "You Have Been Logged Out!")
-    return redirect('home')
